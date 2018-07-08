c1773137dc8c169b57344637ab6460c2a36541c8
zhaozhiqi <442074315@qq.com>
2018-07-08 15:01:45 +0800

    合并base.css

--- rs/b2b/wap/src/pages/Register.vue
+++ rs/b2b/wap/src/pages/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="login">
+    <div id="Register">
         <CommonHeader :commonHeaderObj="commonHeaderObj"></CommonHeader>
         <main class="main">
             <form class="register-form" action="">
@@ -119,113 +119,6 @@ export default {
 </script>
 
 <style scoped>
-#login{
-    height: 100%;
-    overflow: hidden;
-}
 
-.main{
-    position: absolute;
-    top: 100px;
-    left: 0;
-    right: 0;
-    padding: 0 20px;
-}
-
-.register-form{
-    padding: 0;
-}
-
-.register-form label{
-    position: relative;
-    display: block;
-    margin: 30px 0 0;
-    height: 88px;
-    line-height: 88px;    
-    border-radius: 10px;
-    overflow: hidden;
-}
-
-.register-form label.error{
-    border: 1px solid #fa4d3e;
-}
-
-
-.register-form label input{
-    display: block;
-    width: 100%;
-    height: 100%;
-    text-indent: 80px;
-    border: none;
-    background-color: #fff;
-    /* background-color: rgb(250, 255, 189); */
-}
-
-.register-form label .noIndent {
-    text-indent: 20px;
-}
-
-.phoneFirst{
-    position: absolute;
-    top: 24px;
-    left: 16px;
-    height: 40px;
-    line-height: 40px;
-    font-size: 20px;
-    color: #1652b6;
-}
-
-.getCode{
-    position: absolute;
-    top: 0;
-    right: 0;
-    height: 100%;
-    text-align: center;
-    background-color: #1652b6;
-    color: #fff;
-    padding: 0 20px;
-
-}
-
-
-.registerBtn{
-    text-align: center;
-    line-height: 90px;
-    height: 90px;
-    width: 100%;
-    margin: 80px auto 0;
-    display: block;
-    border: 0;
-    border-radius: 10px;
-    overflow: hidden;
-    color: #fff;
-    background-color: #1655bf;
-    font-size: 30px;
-}
-
-.treaty{
-    margin-top: 40px;
-    font-size: 26px;
-}
-
-.treaty input{
-    float: left;
-    margin: 0 20px 0 0;
-}
-
-.treaty a{
-    color: #1655bf;    
-}
-
-.menu{
-    overflow: hidden;
-}
-
-.menu a{
-    float: right;
-    color: #1655bf;
-    font-size: 26px;
-    margin-top: 40px;
-}
 
 </style>

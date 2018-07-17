c66945c7c26a541dee474be82bdb10a9b6794f8b
zhaozhiqi <442074315@qq.com>
2018-07-17 10:43:38 +0800

    no message

--- rs/b2b/wap/src/pages/Group.vue
+++ rs/b2b/wap/src/pages/Group.vue
@@ -5,7 +5,7 @@
       <div class="navItem" :class="{'active':item.active}" @click="changeType(item.id)" v-for="(item, index) in navList" :key="index">{{item.val}}</div>
     </nav>
     <GroupList :proList='proList' :listParent='listParent'/>
-    <Recommend />
+    <!-- <Recommend /> -->
     <Footer />
   </div>
 </template>
@@ -46,7 +46,7 @@ export default {
         },
       ],
       proList:[
-				{bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"../../static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
+				{bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
 				{bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i3/2567690040/TB2GHhSaFqZBuNjt_jqXXamzpXa_!!2567690040.jpg",name:"裙子夏女2018新款韩版露背小心机显瘦小个子ins超火的吊带连衣裙",price:"2222",num:"888",proId:"1"},
 				{bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i3/101126324/TB2WhPbvgmTBuNjy1XbXXaMrVXa_!!101126324.jpg",name:"小番茄定制 连衣裙女夏2018新款假两件拼接收腰裙小心机开叉裙子",price:"21",num:"888",proId:"2"},
 				{bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i2/87731160/TB2bqZjreuSBuNjy1XcXXcYjFXa_!!87731160.jpg",name:"连衣裙夏季2018新款女装韩版显瘦印花雪纺收腰超仙温柔风中长裙子",price:"21",num:"888",proId:"5"},
@@ -67,12 +67,12 @@ export default {
     Recommend
   },
   mounted(){
-    this.setheaderheight();
+    // this.setheaderheight();
   },
   methods:{
-    setheaderheight() {
-      this.bannerHeight = this.$refs.banner.$el.offsetHeight
-    },
+    // setheaderheight() {
+    //   this.bannerHeight = this.$refs.banner.$el.offsetHeight
+    // },
     changeType(id) {
       this.navList.forEach((item)=>{
         if(item.id === id){
@@ -83,7 +83,7 @@ export default {
       })
       if(id === '1'){
         this.proList=[
-          {bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"../../static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
+          {bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
           {bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i3/2567690040/TB2GHhSaFqZBuNjt_jqXXamzpXa_!!2567690040.jpg",name:"裙子夏女2018新款韩版露背小心机显瘦小个子ins超火的吊带连衣裙",price:"2222",num:"888",proId:"1"},
           {bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i3/101126324/TB2WhPbvgmTBuNjy1XbXXaMrVXa_!!101126324.jpg",name:"小番茄定制 连衣裙女夏2018新款假两件拼接收腰裙小心机开叉裙子",price:"21",num:"888",proId:"2"},
           {bable:"新品",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i2/87731160/TB2bqZjreuSBuNjy1XcXXcYjFXa_!!87731160.jpg",name:"连衣裙夏季2018新款女装韩版显瘦印花雪纺收腰超仙温柔风中长裙子",price:"21",num:"888",proId:"5"},
@@ -92,7 +92,7 @@ export default {
         ]
       }else if(id === '2'){
         this.proList=[
-          {bable:"最热",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"../../static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
+          {bable:"最热",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"},
           {bable:"最热",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i3/101126324/TB2WhPbvgmTBuNjy1XbXXaMrVXa_!!101126324.jpg",name:"小番茄定制 连衣裙女夏2018新款假两件拼接收腰裙小心机开叉裙子",price:"21",num:"888",proId:"2"},
           {bable:"最热",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i1/752260885/TB25xdLe_qWBKNjSZFxXXcpLpXa_!!752260885.jpg",name:"妃儿雪纺连衣裙女夏季装2018新款气质显瘦温柔超仙女收腰长裙裙子",price:"21",num:"888",proId:"3"}
         ]
@@ -100,7 +100,7 @@ export default {
         this.proList=[
           {bable:"最新",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i1/752260885/TB25xdLe_qWBKNjSZFxXXcpLpXa_!!752260885.jpg",name:"妃儿雪纺连衣裙女夏季装2018新款气质显瘦温柔超仙女收腰长裙裙子",price:"21",num:"888",proId:"3"},
           {bable:"最新",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"https://gw.alicdn.com/imgextra/i2/757632702/TB2jiRCrf5TBuNjSspmXXaDRVXa_!!757632702.jpg",name:"雪纺连衣裙女夏季显瘦高腰韩国V领女装夏装气质拼接超仙imiss裙子",price:"21",num:"888",proId:"6"},
-          {bable:"最新",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"../../static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"}
+          {bable:"最新",spec:"规格：500ml 单位：1瓶",jobPrice:"99.99",groupPrice:"29.99",mustNum:"100",percent:"50",proImg:"static/images/wap-19.png",name:"ROUWANBABY180601/180602夏天就要穿美裙娃娃款连衣裙闺蜜装",price:"21",num:"888",proId:"4"}
         ]
       }
     }
@@ -108,61 +108,3 @@ export default {
 };
 </script>
 
-<style>
-html {
-  margin: 0;
-  padding: 0;
-  height: 100vh;
-  box-sizing: border-box;
-}
-body {
-  margin: 0;
-  padding: 0;
-  min-height: 100vh;
-  background-color: #f4f4f4;
-}
-
-img {
-  width: 100%;
-  height: auto;
-  vertical-align: top;
-  content: normal !important;
-}
-  [aspectratio] {
-	  position: relative;
-  }
-  [aspectratio]::before {
-	  content: '';
-	  display: block;
-	  width: 1px;
-	  margin-left: -1px;
-	  height: 0;
-  }
-
-  [aspectratio-content] {
-	  position: absolute;
-	  top: 0;
-	  left: 0;
-	  right: 0;
-	  bottom: 0;
-	  width: 100%;
-	  height: 100%;
-  }
-  [flexContainer] {
-    display: flex;
-    width: 750px;
-  }
-  .block {
-    margin-top: 15px;
-  }
-  ul,li {
-    list-style: none outside none;
-    margin: 0;
-    padding: 0;
-  }
-  figure {
-    display: block;
-    margin: 0;
-  }
-
-</style>

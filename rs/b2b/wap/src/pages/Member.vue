a3a532409e8047c32a7bbf24f9f4c6663f1c1bca
zhaozhiqi <442074315@qq.com>
2018-07-17 10:44:07 +0800

    no message

--- rs/b2b/wap/src/pages/Member.vue
+++ rs/b2b/wap/src/pages/Member.vue
@@ -10,7 +10,8 @@
 					<figure><img class="memberLogo" :src="memberInfo.memberLogoImg"></figure>
 					<div class="memberInfoCon">
 						<p class="memberInfoName">{{memberInfo.memberName}}</p>
-						<p class="memberInfoStore">{{memberInfo.memberStoreName}}</p>
+						<!-- <p class="memberInfoStore">{{memberInfo.memberStoreName}}</p> -->
+						<p class="memberInfoRank">{{memberInfo.memberInfoRank}}</p>
 						<p class="memberInfoId">{{memberInfo.memberId}}</p>
 					</div>
 				</div>
@@ -28,18 +29,18 @@
 					</nav>
 				</div>
 				<router-link  
-				:to="{ name: 'Order', params:{ userId: memberInfo.memberId, orderTabActive: 'allOrder'}}" 
+				:to="{ name: item.href, params:{ userId: memberInfo.memberId, orderTabActive: 'allOrder'}}" 
 				class="itemLink" 
-				v-for="(item, index) in 4" 
+				v-for="(item, index) in memberMenuList" 
 				:key="index">
 					<span class="linkName">
-						<i class="rsiconfont rsicon-quanbudingdan"></i><em>全部订单</em>
+						<i class="rsiconfont rsicon-radio"></i><em>{{item.name}}</em>
 					</span>
-					<span class="linkRouter"><em>菜单</em><i class="rsiconfont rsicon-qiehuanqiyou"></i></span>
+					<span class="linkRouter"><em>{{item.con}}</em><i class="rsiconfont rsicon-qiehuanqiyou"></i></span>
 				</router-link>
 			</section>
-			<div class="car-member-recommend">为您推荐</div> 
-			<TypeGoodsList class=""/>
+			<!-- <div class="car-member-recommend">为您推荐</div> 
+			<TypeGoodsList /> -->
 			<Footer />
 		</div>
 	</div>
@@ -48,17 +49,20 @@
 <script>
 import TypeGoodsList from '@/components/TypeGoodsList';
 import Footer from '@/components/Footer';
-
+import Cookies from 'js-cookie'
 
 export default {
 	name: 'Member',
 	data(){
 		return {
+			RANK:"",
 			memberInfo:{
 				memberId:"0001",
-				memberName:"二哈",
-				memberStoreName:"二哈宠物店",
-				memberLogoImg:"../../static/images/memberLogo.jpg"
+				memberName:"王XX",
+				memberInfoRank:"用户",
+				memberInfoStore:"",
+				memberStoreName:"和顺批发",
+				memberLogoImg:"static/images/memberLogo.jpg"
 			},
 			orderMenuList:[
 				{
@@ -92,6 +96,7 @@ export default {
 					num:5
 				}
 			],
+			memberMenuList:[],
 			proinfo: []
 		}
 	},
@@ -109,185 +114,98 @@ export default {
 					item.num = num
 					return
 				}
-			})
-			
+			})			
 		})
-		let RANK = Cookies.get('RANK'); 
-		console.log(RANK,'RANK')
+		this.RANK = Cookies.get('RANK')
+		switch (this.RANK) {
+			case 'admin1':	this.memberInfo.memberInfoRank =  "经销商";
+							this.memberMenuList=[
+									{
+										href:'',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的客户",
+										con:"",
+										num:1
+									},{
+										href:'Seach',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的商品",
+										con:"",
+										num:1
+									},{
+										href:'Order',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的订单",
+										con:"",
+										num:1
+									}
+							]
+				break;
+			case 'admin2':	this.memberInfo.memberInfoRank =  "分销商";
+							this.memberMenuList=[
+									{
+										href:'/',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的客户",
+										con:"",
+										num:1
+									},{
+										href:'Seach',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的商品",
+										con:"",
+										num:1
+									},{
+										href:'Order',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的订单",
+										con:"",
+										num:1
+									}
+							]
+				break;
+			case 'admin3':	this.memberInfo.memberInfoRank =  "超市";
+							this.memberMenuList=[
+									{
+										href:'/',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的客户",
+										con:"",
+										num:1
+									},{
+										href:'Seach',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的商品",
+										con:"",
+										num:1
+									},{
+										href:'Order',
+										iconClass:"rsicon-31daifukuan",
+										name:"我的订单",
+										con:"",
+										num:1
+									}
+							]			
+				break;
+			case 'admin4':	this.memberInfo.memberInfoRank =  "用户";	
+							this.memberMenuList=[
+									{
+										href:'',
+										iconClass:"rsicon-31daifukuan",
+										name:"XXXXXXX",
+										con:"",
+										num:1
+									}
+							]		
+				break;		
+			default:
+				break;
+		}
+		console.log(this.RANK,'RANK');
+
 	},
 	methods:{
 
 	}
 };
 </script>
-
-<style scoped>
-#member{
-	overflow: hidden;
-	/* background: #fff; */
-	height: 100%;
-}
-
-.main{
-	position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	padding-bottom: 100px;
-	overflow: hidden;
-}
-
-.header{
-	flex-direction: column;
-	background: #1655bf;
-	color: #fff;
-	height: 220px;
-	padding-top: 20px;
-	padding-bottom: 20px;
-	
-}
-
-.header .top{
-	overflow: hidden;
-	width: 100%;
-	height: 60px;
-	line-height: 60px;
-}
-
-.header .noticeListLink,.header .memberSetLink{
-	float: right;
-	text-align: center;
-	margin-right: 20px;
-	width: 60px;
-	height: 60px;
-}
-
-.memberInfo{
-	padding: 0 20px 0 40px;
-	width: 100%;
-	height: 120px;
-	overflow: hidden;
-}
-
-.memberInfo figure{
-	float: left;
-	width: 100px;
-	height: 100px; 
-	margin-right: 40px;
-	border-radius: 50%;
-	overflow: hidden;
-}
-.memberInfo figure{
-	width: 120px;
-	height: 120px; 
-	border-radius: 50%;
-	overflow: hidden;
-}
-
-.memberInfo .memberInfoCon{
-	float: left;
-}
-
-.memberInfoName,.memberInfoStore,.memberInfoId{
-	font-size: 30px;
-	line-height: 40px;
-	height: 40px;
-}
-
-.memberInfoId{
-	font-size: 20px; 
-}
-
-.itemLink{
-	background-color: #fff; 
-	padding: 0 20px;
-	overflow: hidden;
-	display: block; 
-	width: 100%;
-	line-height: 80px;
-	height: 80px;
-	font-size: 25px;
-	vertical-align: middle;
-	border-bottom: 1px solid #efefef;
-}
-
-.itemLink .rsiconfont{
-	font-size: 20px;
-}
-
-.itemLink .linkName{
-	float: left;
-}
-.itemLink .linkRouter{
-	float: right;
-}
-
-.itemLink .linkName .rsiconfont{
-	color: #fa4d3e;
-	font-size: 40px;
-	margin-right: 10px;
-}
-
-.itemLink .linkRouter .rsiconfont{
-	margin-left: 10px;
-}
-
-.orderMenu{
-	background-color: #fff;
-	margin-bottom: 20px;
-}
-
-.orderMenu nav{
-	display: flex;
-}
-
-.orderMenu nav .orderMenuLink{
-	position:relative;
-	flex: 1;
-	padding: 20px 0;
-	display: flex;
-	flex-direction: column;
-	text-align: center
-}
-
-.orderMenu nav .orderMenuLink .rsiconfont{
-	display: block;
-	line-height: 48px;
-	height: 48px;
-	font-size: 44px;
-}
-
-.orderMenu nav .orderMenuLink b{
-	height: 1px;
-	margin: 0 auto;
-	min-width: 60px;
-    display: block;
-    background-color: #fa4d3e;
-}
-
-.orderMenu nav .orderMenuLink em{
-	position: absolute;
-    top: 20px;
-    left: 56%;
-    display: block;
-	width:30px;
-	height: 30px;
-	line-height: 30px;
-	text-align: center;
-	background-color: #fa4d3e;
-	font-size: 22px;
-	color: #fff;
-	border-radius: 50%;
-	overflow: hidden;
-}
-
-
-.car-member-recommend{
-	line-height: 40px;
-	height: 40px;
-	margin: 20px 0;
-	text-align: center;
-	font-size: 30px;
-	font-weight: bold;
-}
-</style>
